f661ce19bda5b2c91075f4011acbddaa7da77524
thierno <thiernobah91860@yahoo.fr>
2023-11-29 16:30:47 +0100

    feat(calculate) : return simple number input

---
+++ src/main/kotlin/uk/zinch/kata/RPNCalculator.kt
@@ -0,0 +1,8 @@
+package uk.zinch.kata
+
+class RPNCalculator {
+
+  fun calculate(expression: String) : Int =
+    expression.toInt()
+
+}
--- src/test/kotlin/uk/zinch/kata/TemplateTest.kt
+++ src/test/kotlin/uk/zinch/kata/TemplateTest.kt
@@ -5,14 +5,38 @@ import io.kotest.matchers.shouldBe
 
 class TemplateTest: ShouldSpec(
   {
-    context("multiply function") {
-      should("return 4 for input 2x2") {
-        // given
-        val template = Template()
+      should("return 4 for input 4") {
         // when
-        val result = template.multiply(2, 2)
+        val result = RPNCalculator().calculate("4")
         // then
         result shouldBe 4
       }
+
+    should("return 5 for input 5") {
+      // when
+      val result = RPNCalculator().calculate("5")
+      // then
+      result shouldBe 5
+    }
+
+    should("return 3 for input 3") {
+      // when
+      val result = RPNCalculator().calculate("3")
+      // then
+      result shouldBe 3
+    }
+
+    should("return 7 for input 7") {
+      // when
+      val result = RPNCalculator().calculate("7")
+      // then
+      result shouldBe 7
+    }
+
+    should("return 12 for input 12") {
+      // when
+      val result = RPNCalculator().calculate("12")
+      // then
+      result shouldBe 12
     }
   })
